5e3a03bc695e053033b9686015c8d9fac406d1ba
Emile Wong <emilewong@outlook.com>
2021-06-26 14:35:14 +0800

    Initial Commit

---
+++ NotesOnAppleWatch WatchKit Extension/NotesOnAppleWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesOnAppleWatchApp.swift
+//  NotesOnAppleWatch WatchKit Extension
+//
+//  Created by Emile Wong on 26/6/2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesOnAppleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
